8324e494b08778c417ec7aceec69263d712d4a65
mydde <lebrun.meddy@gmail.com>
2021-04-21 18:38:35 +0200

    done
    mend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">css-fabric!</a>
+          Welcome to <a href="">css-fabric!</a>
         </h1>
         <p className={styles.description}>
            
